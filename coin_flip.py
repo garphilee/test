353bf49489f0f3fd7fd3db2cf681845411349717
garphilee <68087684+garphilee@users.noreply.github.com>
2022-01-10 17:40:14 -0500

    Create coin_flip.py

---
+++ coin_flip.py
@@ -0,0 +1,80 @@
+import random
+import matplotlib
+
+def flip():
+    global coin
+    #flip coin
+    flip_coin = random.randint(0,1)
+    if flip_coin == 0:
+        coin = "Tails"
+    else:
+        coin = "Heads"
+    return coin
+
+"""
+1. flip coin
+2. if heads, keep flipping until tails.
+3. record number of consecutive Heads, save - ie. this run had 1 Head, next run had 2, etc...
+4. Reset head + tail head count
+5. keep tally of all consecutive runs of heads - "there were 5 runs that had only 1 consecutive head, 3 runs with 2 consecutive heads, etc..."
+"""
+
+def consec_heads(runs):
+    #go through specified number of times (runs)
+    #tail_count = 0
+    head_count = 0
+    total_head_count = 0
+    record = []
+    #for graphing purposes
+    numbers = []
+    lables = []
+    while runs > 0:
+        flip()
+        if coin == "Heads":
+            print("flipped heads")
+            head_count += 1
+            total_head_count += 1
+        else:
+            print("flipped tails")
+            print("You flipped " + str(head_count) + " consecutive Heads!")
+            print("You are done flipping.")
+            record.append(head_count)
+            head_count = 0
+            #tail_count += 1
+            runs -= 1
+    print("------------------")
+    print("Statistics: ")
+    print("Record of flips: " + str(record))
+    print("Max consecutive Head streak: " + str(max(record)))
+    print("Average streak: " +str((sum(record)/len(record))))
+    #total number of heads versus tails
+    print("Total number of Heads flipped: " +str(total_head_count))
+
+    # find occurances of each item(unique streak) in list
+    ## starting with highest streak, count how many times that streak has occured
+
+    # finding max streak
+    list_count = max(record)
+    #print("List Count is: " + str(list_count))
+    # from highest streak, go down by 1 streak until zero, count number of occurences.
+    while list_count > -1:
+        print("'" + str(list_count) + "'" + " occured " + str(record.count(list_count)) + " many times.")
+        """
+        numbers.append(record.count(list_count))
+        labels.append(list_count)
+        print(numbers)
+        print(labels)
+        """
+        list_count -= 1
+
+    # ------------------------ #
+    # matplotlib
+    ## bar chart of occurances
+    def bar_chart(numbers,labels,pos):
+        plt.bar(pos,numbers,color='blue')
+        plt.xticks(ticks=pos,labels=labels)
+        plt.show()
+
+
+
+consec_heads(20)
